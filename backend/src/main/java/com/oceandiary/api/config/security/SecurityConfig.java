15d3b8add9d90d7cab735bcb8da0c8a82f31aa23
jiyeon-agnes-lee <jy.agnes.lee@gmail.com>
2022-08-04 11:30:22 +0900

    feat: Security, Jwt 설정 파일 생성 (#22)

---
+++ backend/src/main/java/com/oceandiary/api/config/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.oceandiary.api.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 목적 : 서버에 보안 설정 적용(리소스 접근 가능 여부 세팅)
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests() // 사용권한 체크
+                .antMatchers("/docs/**").permitAll() // restdocs 주소는 누구나 접근 가능 (TODO: swagger 적용시 수정 필요)
+                .antMatchers("/*/login").permitAll() // 가입 및 인증 주소는 누구나 접근 가능
+                .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // jwt token 필터를 id/password 인증 필터 전에 추가
+
+    }
+}
