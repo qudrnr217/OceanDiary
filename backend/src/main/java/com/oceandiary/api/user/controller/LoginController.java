aae471f21fb30e335ebb37ee08665e12af110031
minhaeun <haeunee6575@gmail.com>
2022-08-09 11:23:41 +0900

    refactor: 소셜로그인 중복 코드 제거 (#10)

--- backend/src/main/java/com/oceandiary/api/user/controller/LoginController.java
+++ backend/src/main/java/com/oceandiary/api/user/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
         // 상태 토큰은 추후 검증을 위해 세션에 저장되어야 한다.
         // 상태 토큰으로 사용할 랜덤 문자열 생성
         String state = new BigInteger(130, new SecureRandom()).toString(32);
-        ;
+        
         // 세션 또는 별도의 저장 공간에 상태 토큰을 저장
         session.setAttribute("state", state);
         log.info("session={}", session.getAttribute("state"));
@@ -44,13 +44,13 @@ public class LoginController {
 
     // TODO: 수정하기
     @PostMapping("/kakao/login")
-    public LoginResponse loginByKakao(LoginRequest request, HttpSession session) {
+    public LoginResponse loginByKakao(LoginRequest request) {
         log.info("code = {}", request.getCode());
-        return loginService.naverLogin(request, session);
+        return loginService.kakaoLogin(request);
     }
 
-    @PostMapping("/*/signup")
-    public JoinResponse joinByKakao(@RequestBody @Valid JoinRequest request) {
-        return loginService.join(request);
+    @PostMapping("/{provider}/signup")
+    public JoinResponse join(@RequestBody @Valid JoinRequest request, @PathVariable String provider) {
+        return loginService.join(request, provider);
     }
 }
--- backend/src/main/java/com/oceandiary/api/user/service/LoginService.java
+++ backend/src/main/java/com/oceandiary/api/user/service/LoginService.java
@@ -2,6 +2,8 @@ package com.oceandiary.api.user.service;
 
 import com.oceandiary.api.user.domain.Role;
 import com.oceandiary.api.user.domain.SocialProvider;
+import com.oceandiary.api.user.dto.KakaoApiResponse;
+import com.oceandiary.api.user.dto.NaverApiResponse;
 import com.oceandiary.api.user.entity.User;
 import com.oceandiary.api.user.repository.SocialLoginUserRepository;
 import com.oceandiary.api.user.request.JoinRequest;
@@ -12,10 +14,17 @@ import com.oceandiary.api.user.security.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpSession;
+import java.net.URI;
 
 @Service
 @Transactional(readOnly = true)
@@ -28,24 +37,34 @@ public class LoginService {
 
     // TODO: @ConfigurationProperties 혹은 @Value 사용해서 API 키 감추기
     @Value("${NAVER_API_CLIENT_ID}")
-    private String clientId;
+    private String naverClientId;
     @Value("${NAVER_API_CLIENT_SECRET}")
-    private String clientSecret;
+    private String naverClientSecret;
+    @Value("${KAKAO_API_CLIENT_ID}")
+    private String kakaoClientId;
 
     public LoginResponse naverLogin(LoginRequest request, HttpSession session){
-        String accessToken = getAccessToken(request, session);
+        String accessToken = getNaverAccessToken(request, session);
         String naverUniqueId = getNaverUniqueId(accessToken);
 
         User foundUser = socialLoginUserRepository.findByProviderAndOauthId(SocialProvider.NAVER, naverUniqueId);
         return checkUser(foundUser, naverUniqueId);
     }
 
-    public JoinResponse join(JoinRequest request){
+    public LoginResponse kakaoLogin(LoginRequest request){
+        String accessToken = getKakaoAccessToken(request);
+        String kakaoUniqueId = getKakaoUniqueId(accessToken);
+        User foundUser = socialLoginUserRepository.findByProviderAndOauthId(SocialProvider.KAKAO, kakaoUniqueId);
+        return checkUser(foundUser, kakaoUniqueId);
+    }
+
+    public JoinResponse join(JoinRequest request, String provider){
+        SocialProvider socialProvider = (provider.equals(SocialProvider.NAVER)) ? SocialProvider.NAVER:SocialProvider.KAKAO;
         User newUser = User.builder()
                 .email(request.getEmail())
                 .name(request.getName())
                 .birth(request.getBirth())
-                .provider(SocialProvider.NAVER)
+                .provider(socialProvider)
                 .role(Role.USER)
                 .oauthId(request.getOauthId())
                 .build();
@@ -63,8 +82,6 @@ public class LoginService {
 
         return response;
     }
-
-
 
     public LoginResponse checkUser(User foundUser, String uniqueId){
         LoginResponse response;
@@ -84,11 +101,91 @@ public class LoginService {
         return response;
     }
 
-    public String getAccessToken(LoginRequest request, HttpSession session){
-        return null;
+    public String getNaverAccessToken(LoginRequest request, HttpSession session){
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://nid.naver.com")
+                .path("/oauth2.0/token")
+                .queryParam("grant_type", "authorization_code")
+                .queryParam("client_id", naverClientId)
+                .queryParam("client_secret", naverClientSecret)
+                .queryParam("code", request.getCode())
+                .queryParam("state", session.getAttribute("state"))
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<NaverApiResponse.AccessTokenApiResponse> responseEntity
+                = restTemplate.getForEntity(uri, NaverApiResponse.AccessTokenApiResponse.class);
+        log.info(responseEntity.getBody().getAccessToken());
+        return responseEntity.getBody().getAccessToken();
+    }
+
+    public String getKakaoAccessToken(LoginRequest request) {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://kauth.kakao.com")
+                .path("/oauth/token")
+                .queryParam("grant_type", "authorization_code")
+                .queryParam("client_id", kakaoClientId)
+                .queryParam("redirect_uri", "http://localhost:8080")
+                .queryParam("code", request.getCode())
+                .encode()
+                .build()
+                .toUri();
+
+        //System.out.print("uri: " + uri);
+        log.info("uri : " + uri );
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<NaverApiResponse.AccessTokenApiResponse> responseEntity
+                = restTemplate.getForEntity(uri, NaverApiResponse.AccessTokenApiResponse.class);
+        log.info(responseEntity.getBody().getAccessToken());
+        return responseEntity.getBody().getAccessToken();
     }
 
     public String getNaverUniqueId(String accessToken){
-        return null;
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/nid/me")
+                .encode()
+                .build()
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        HttpEntity request = new HttpEntity(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<NaverApiResponse.ProfileApiResponse> responseEntity = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                request,
+                NaverApiResponse.ProfileApiResponse.class
+        );
+        log.info("NAVER Unique ID = {}", responseEntity.getBody().getResponse().getId());
+        return responseEntity.getBody().getResponse().getId();
     }
+
+    public String getKakaoUniqueId(String accessToken) {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://kapi.kakao.com")
+                .path("/v2/user/me")
+                .encode()
+                .build()
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        HttpEntity request = new HttpEntity(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<KakaoApiResponse.ProfileApiResponse> responseEntity = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                request,
+                KakaoApiResponse.ProfileApiResponse.class
+        );
+        log.info("KAKAO Unique ID = {}", responseEntity.getBody().getId());
+        return responseEntity.getBody().getId().toString();
+    }
+
 }
